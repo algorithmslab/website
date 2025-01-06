8f36cb290bf2d637dc9d30ee69a802db83c984f4
ali safarpoor dehkordi <37835450+aSafarpoor@users.noreply.github.com>
2025-01-06 15:46:39 +1100

    Update background.js

--- js/background.js
+++ js/background.js
@@ -12,7 +12,7 @@ function resizeCanvas() {
 resizeCanvas();
 
 // Number of circles (nodes)
-const particleCount = 20;
+const particleCount = 20000;
 // Max distance for drawing connections
 const connectionDistance = 150;
 // Array to store our circles
